2e4069b87996d0788e1bfc670441b6293be35747
= <thechrishanson@yahoo.com>
2018-11-29 15:56:37 -0700

    Save files

--- EducationalApp/world.cpp
+++ EducationalApp/world.cpp
@@ -4,15 +4,10 @@
 World::World(float32 gravity, float32 timeStep)
 {
     this->timeStep = timeStep;
-<<<<<<< HEAD
     gravityF = b2Vec2(0.0f, gravity);
     //b2World newWorld(gravityF);
     //world = newWorld;
     world(gravityF);
-=======
-    b2Vec2 gravityF(0.0f, gravity);
-    world = new b2World(gravityF);
->>>>>>> 05768446477ef7617d46e44e662d412cf09d2007
 }
 
 Shape World::createBox(float32 width, float height, float32 posX, float32 posY)
