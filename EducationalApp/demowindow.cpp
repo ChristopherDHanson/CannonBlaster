cfb9b1d04ae3a1ab6edbe9a26be91540b345b4e1
= <thechrishanson@yahoo.com>
2018-11-30 15:38:32 -0700

    Add texture loading and storage in level, make corresponding changes in demowindow, where levels are constructed

--- EducationalApp/demowindow.cpp
+++ EducationalApp/demowindow.cpp
@@ -20,8 +20,22 @@ DemoWindow::DemoWindow(QWidget *parent) :
 
 
     // CONSTRUCT LEVELS--------
+    // General process is: 1) Construct level, 2) Load its textures, 3) Add elements
     // Level 1:
     Level* level1 = new Level(-10.0f, 4.0f / 100.0f);
+
+    textures = std::vector<sf::Texture>(2);
+    if (!textures[0].loadFromFile("../icon.png")) {
+        throw "EXIT_FAILURE";
+    }
+
+    if (!textures[1].loadFromFile("../icon2.png")) {
+        throw "EXIT_FAILURE";
+    }
+    textures[0].setSmooth(true);
+    textures[1].setSmooth(true);
+    level1->loadTextures(textures);
+
     level1->createBox(40, 20, 10, 10);
     levels.push_back(level1);
 
--- EducationalApp/level.cpp
+++ EducationalApp/level.cpp
@@ -8,8 +8,16 @@ Level::Level(float32 gravity, float32 timeStep)
     world = new b2World(gravityF);
 }
 
+void Level::loadTextures(std::vector<sf::Texture> inputTextures) {
+    textures = inputTextures;
+}
+
 void Level::createBox(float32 width, float height, float32 posX, float32 posY)
 {
+    if (textures.size() == 0) {
+        throw std::runtime_error("Textures array empty");
+    }
+    // Set up physical box
     b2BodyDef bodyDef;
     bodyDef.position.Set(posX, posY);
     b2Body* body = world->CreateBody(&bodyDef);
@@ -17,6 +25,13 @@ void Level::createBox(float32 width, float height, float32 posX, float32 posY)
     firstBox.SetAsBox(width/2, height/2);
     body->CreateFixture(&firstBox, 0.0f);
     bodies.push_back(body);
+
+    // Set up visual box
+    sf::Sprite* sprite = new sf::Sprite(textures[0]);
+
+    sprite->setOrigin(posX,posY);
+    sprite->setPosition(0,0);
+    sprites.push_back(sprite);
 }
 
 void Level::next()
